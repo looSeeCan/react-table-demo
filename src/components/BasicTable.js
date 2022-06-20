ef132e9f4a30c2d9581cc9d22cfc862e71a3ba73
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-19 23:50:35 -0500

    React Table Tutorial 3
    7:16

---
+++ src/components/BasicTable.js
@@ -0,0 +1,18 @@
+//STEP 3: WE NEED TO CREATE A TABLE INSTANCE
+//this is where we are going to make use of the react table library
+//to create a new table component we need to import 3 things
+
+import React from 'react';
+import {useTable} from "react-table";
+import MOCK_DATA from "./MOCK_DATA (1).json";
+import {COLUMNS} from "./columns";
+
+const BasicTable = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BasicTable;
