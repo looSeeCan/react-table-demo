923d059a94539e19d96d4378cd72cd07776a2c4c
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-20 13:57:01 -0500

    got a table
    finished with React Table Tutorial 3. I am going to stop here and see if I can incorporate this into cycle10rfp.

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -2,25 +2,71 @@
 //this is where we are going to make use of the react table library
 //to create a new table component we need to import 3 things
 
-import React from 'react';
-import {useTable} from "react-table";
-import MOCK_DATA from "./MOCK_DATA (1).json";
-import {COLUMNS} from "./columns";
+import React, {useMemo} from 'react';
+import { useTable } from 'react-table';
+import MOCK_DATA from "./MOCK_DATA (1).json";//the mock data that we created
+import {COLUMNS} from "./columns";//the array of columns that we have just defined in columns
+import "./table.css";
 
 export const BasicTable = () => {
     const columns = useMemo(() => COLUMNS, []);
     const data = useMemo(() => MOCK_DATA, []);
+    // console.log("hello");
 
-    useTable({
-        columns: COLUMNS,
-        data: MOCK_DATA
+    //CREATE A TABLE INSTANCE
+    const tableInstance = useTable({//useTable hook.. We pass in an object as argument 
+        // columns: COLUMNS,//we specify two properties, columns and rows
+        // data: MOCK_DATA
+
+        columns,//shorthand 
+        data
     })
 
+    //STEP 5: we need to use the table instance with our jsx to render all necessary UI
+    //we begin by destructuring a couple of properties and methods from the table instance
+
+    const {//these are basically functions and arrays that the useTable hook from react table package has given to us to eanle easy table creation
+        getTableProps,//this is a function that needs to be destructured on the table tag
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance
+
     return (
-        <div>
-            
-        </div>
+      //STEP 4: define a basick table structure just using html
+        //with in both the thead and the tbody, we need a tr to specify a row
+        //in the thead the data is wrapped with a th tag and in the body, the data is wrapped with td tag
+    <table {...getTableProps()}>
+        <thead>
+            {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                        <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                    ))}
+                </tr>    
+            ))}
+            <tr>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                            return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                        })}
+                    </tr>
+                )
+            })}
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>      
     );
 }
 
-export default BasicTable;
+// export default BasicTable;
